92c89a64406a0051f5ef55ccbf0b7ba4229b7ca5
Subham Sengupta <sengupta335@gmail.com>
2018-04-02 11:44:35 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const params = function (url) {
+  url = url.split('?')[1].split('&')
+  url = url.reduce(function (acc, cur) {
+    let key = cur.split('=')[0]
+    let val = cur.split('=')[1]
+    acc[key] = val
+    return acc
+  }, {})
+  return url
+}
+export default params
